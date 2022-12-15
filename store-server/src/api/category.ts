02eb1693d2a03ec84dbb8cc9d2cc603efccfa4da
psu6510110132 <natchalpk@gmail.com>
2022-12-15 20:44:12 +0700

    Backend ของ STEP1

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -1,11 +1,13 @@
 import Router from 'koa-router'
+import db from '../db'
 const router = new Router()
 
-router.get('/',async (ctx, next) => {
-    ctx.body = [
-        {id: 1, title: 'Computer'},
-        {id: 2, title: 'Fantasy'}
-    ]
+const makeQuery = () => db('category').select('*')
+const findById = (id: number) => makeQuery().where({id})
+
+router
+  .get('/', async (ctx, next) => {            
+    ctx.body = await makeQuery().orderBy('id')
 })
 
 export default router
---
+++ store-server/src/index.ts
@@ -0,0 +1,8 @@
+import Koa from 'koa'
+import apiRouter from './api'
+
+const app = new Koa()
+
+app.use(apiRouter.routes())
+
+app.listen(8000)
