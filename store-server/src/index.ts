8cfe48f7a92f2cdc88424db0e61acb69f15a073f
psu6510110132 <natchalpk@gmail.com>
2022-12-15 21:29:47 +0700

    สร้าง Fixtures.ts

--- store-server/src/index.ts
+++ store-server/src/index.ts
@@ -1,8 +1,12 @@
 import Koa from 'koa'
 import apiRouter from './api'
+import loadFixtures from './fixtures'
+import appConfig from './config'
 
 const app = new Koa()
 
 app.use(apiRouter.routes())
 
 app.listen(8000)
+
+loadFixtures(appConfig.clearDataBeforeLodingFixture)
