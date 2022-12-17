66dbf56c70d5e06855a26ae0f8f1f282191b01c6
psu6510110132 <natchalpk@gmail.com>
2022-12-17 17:27:11 +0700

    Backend ของ STEP2 (แก้ไข)

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -8,14 +8,14 @@ async function loadFixtures(clearData = false){
   }
 
   await db.batchInsert('category', [
-    { id: 1, title: 'Fantasy' },
-    { id: 2, title: 'Computer' },
+    { id: 1, title: 'Computer' },
+    { id: 2, title: 'Fantasy' },
   ])
   
   await db.batchInsert('book', [
-    { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 1 },
-    { id: 2, title: 'Game of Thrones', price: 520, stockAmount: 6, categoryId: 1 },
-    { id: 3, title: 'Node.js', price: 300, stockAmount: 4, categoryId: 2 },
+    { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 2 },
+    { id: 2, title: 'Game of Thrones', price: 520, stockAmount: 6, categoryId: 2 },
+    { id: 3, title: 'Node.js', price: 300, stockAmount: 4, categoryId: 1 },
   ])
 }
 
