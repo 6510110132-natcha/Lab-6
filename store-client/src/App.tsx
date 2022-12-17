7c8fc4aadc2910c24037e6d4152f4b2178d06d66
psu6510110132 <natchalpk@gmail.com>
2022-12-17 20:15:29 +0700

    ใช้ component BookDetail ในการแสดงข้อมูลที่mockไว้

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from 'react'
+import BookDetail from './components/BookDetail'
+import Book from './models/Book'
 import Category from './models/Category'
 import Repo from './repositories'
 
 function App() {
+  const [bookList, setBookList] = useState<Book[]>([])
   const [categoryList, setCategoryList] = useState<Category[]>([])
 
+
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll()
+    if (result) {
+      setBookList(result)
+    }
+  }
 
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
@@ -12,15 +22,27 @@ function App() {
       setCategoryList(result)
     }
   }
-
+  
 
   useEffect(() => {
     fetchCategoryList()
-  })
+    fetchBookList()
+  },[])
 
   return (
     <div>
-      {categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+    <div>
+         {categoryList.map(category => 
+           <option key={category.id} value={category.id}>{category.title}</option>)}
+        <hr />
+      </div>
+      {bookList.map(book =>
+        <div key={book.id}>
+          <BookDetail {...book} />
+          <hr />
+        </div>
+      )}
+
     </div>
   );
 }
